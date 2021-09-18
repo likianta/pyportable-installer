b76e5a902cc9fbf915d0c7e4772ffbd2d46772c9
Likianta <likianta@foxmail.com>
2021-09-18 10:24:58 +0800

    Fix `ASSETS_ENTRY` check

--- pyportable_installer/_env.py
+++ pyportable_installer/_env.py
@@ -13,7 +13,8 @@ if ASSETS_ENTRY == 'AUTO_DETECT':
     _name = basename(abspath(f'{__file__}/../..'))
     if _name == 'site-packages':
         ASSETS_ENTRY = 'PACKAGE'
-    elif _name.startswith(('pyportable_installer', 'pyportable-installer')):
+    elif _name.startswith(('pyportable_installer', 'pyportable-installer')) \
+            or _name == 'src':  # 'src' is appeared when dist as an application.
         ASSETS_ENTRY = 'STANDALONE'
     else:
         raise EnvironmentError('Auto detect failed to locate pyportable-'
